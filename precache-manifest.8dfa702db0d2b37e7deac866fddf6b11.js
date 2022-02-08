21be1731129d263f07cc32177a61d903f09286fd
Code Sandbox <hello@codesandbox.io>
2022-02-08 11:26:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8dfa702db0d2b37e7deac866fddf6b11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5435c097d3d0c026a41bd50f5ff019e0",
+    "url": "https://haseenaisahibraheem.github.io/csb-wf4j6/index.html"
+  },
+  {
+    "revision": "239339a365d2a1c6864e",
+    "url": "https://haseenaisahibraheem.github.io/csb-wf4j6/static/js/2.be24c9c8.chunk.js"
+  },
+  {
+    "revision": "20084c925a8c4433771b",
+    "url": "https://haseenaisahibraheem.github.io/csb-wf4j6/static/js/main.61ad8700.chunk.js"
+  },
+  {
+    "revision": "98de5c04ae926645b23f",
+    "url": "https://haseenaisahibraheem.github.io/csb-wf4j6/static/js/runtime~main.23460f13.js"
+  }
+]);
